51f9751afdecd526e94f922cd030010c8ddb6077
mouse <m1689141525@gmail.com>
2021-03-23 15:21:07 +0800

    Added test

--- src/test/java/com/mouse/leetcode/FindMedianSortedArraysTest.java
+++ src/test/java/com/mouse/leetcode/FindMedianSortedArraysTest.java
@@ -11,13 +11,17 @@ class FindMedianSortedArraysTest {
         FindMedianSortedArrays findMedianSortedArrays = new FindMedianSortedArrays();
 
         assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{2, 3}, new int[]{6, 7})).isEqualTo(4.5);
-        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{2}, new int[]{6, 7})).isEqualTo(6);
-
-        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{1, 3}, new int[]{2})).isEqualTo(2);
         assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4})).isEqualTo(2.5);
         assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{0, 0}, new int[]{0, 0})).isEqualTo(0);
-        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{}, new int[]{1})).isEqualTo(1);
         assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{2}, new int[]{})).isEqualTo(2);
         assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{2, 5}, new int[]{3, 4})).isEqualTo(3.5);
+
+        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{1, 3, 5, 7, 9}, new int[]{2, 4, 6, 8})).isEqualTo(5);
+        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{1, 5, 6, 7, 8}, new int[]{2, 4, 6, 8})).isEqualTo(6);
+        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{2}, new int[]{6, 7})).isEqualTo(6);
+        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{1, 3}, new int[]{2})).isEqualTo(2);
+        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{}, new int[]{1})).isEqualTo(1);
+        assertThat(findMedianSortedArrays.findMedianSortedArrays(new int[]{1, 3, 5, 7, 9}, new int[]{2, 4, 6, 8})).isEqualTo(5);
+
     }
 }
